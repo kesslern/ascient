fa8826fc66a1aa85c41787f9591f5f5e0f7d83e9
Nathan Kessler <nathant93@gmail.com>
2018-10-28 11:15:10 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Use an official Python runtime as a parent image
+FROM anapsix/alpine-java:8_jdk
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+EXPOSE 8080
+
+CMD ["./gradlew", "run"]
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+package us.kesslern.ascient
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
