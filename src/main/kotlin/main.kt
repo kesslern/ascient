939480b082b0c9ef045d043a138a02722dd3de52
Nathan Kessler <nathant93@gmail.com>
2018-10-31 22:59:50 -0400

    Add basic db connection

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,12 +7,40 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object booleans : Table() {
+    val id: Column<Int> = integer("id").autoIncrement().primaryKey()
+    val name: Column<String> = varchar("name", 25)
+    val value: Column<Boolean> = bool("value")
+}
 
 fun main(args: Array<String>) {
+
+    Database.connect(
+            "jdbc:postgresql://localhost:5432/postgres?user=user&password=pass",
+            driver = "org.postgresql.Driver")
+
     embeddedServer(Netty, 8080) {
         routing {
             get("/api/test") {
-                call.respondText("My Example Blog 4", ContentType.Text.Html)
+
+                var result: Boolean? = null
+                transaction {
+                    booleans.select {
+                        booleans.name eq "test"
+                    }.forEach { result = it[booleans.value] }
+                }
+
+                if (result != null) {
+                    call.respondText("It was ${result}", ContentType.Text.Html)
+                } else {
+                    call.respondText("no bro", ContentType.Text.Html)
+                }
             }
         }
     }.start(wait = true)
---
+++ src/sql/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE booleans (
+  id              SERIAL PRIMARY KEY,
+  name            VARCHAR(25) NOT NULL,
+  value           BOOLEAN NOT NULL
+);
