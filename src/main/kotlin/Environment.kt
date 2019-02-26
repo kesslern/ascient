6a914b613ab400518ea3574ef6ef9590e6b91be9
kesslern <nathant93@gmail.com>
2019-02-25 21:58:11 -0500

    Remove unused environment variable

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -6,7 +6,5 @@ object Environment {
     val databasePassword: String = System.getProperty("database.password", "")
     val databasePort: Int = System.getProperty("ascient.port", "8080").toInt()
     val purgeInterval: Long = System.getProperty("ascient.sessions.purgeInterval", "300").toLong() * 1000
-    val password: String = System.getProperty("ascient.sessions.password", "please")
     val sessionLength: Int = System.getProperty("ascient.sessions.length", "600").toInt()
-
 }
